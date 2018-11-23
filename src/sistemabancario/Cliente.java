a856c6a652a593460a110d5f4672766100d47125
William Bigas Mauro <williambmauro@hotmail.com>
2018-11-23 06:20:28 -0200

    Upload inicial

---
+++ src/sistemabancario/Cliente.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemabancario;
+
+/**
+ *
+ * @author Will-PC
+ */
+public class Cliente {
+
+    private String nome;
+    private Integer agencia;
+    private Integer conta;
+    private double saldo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(Integer agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getConta() {
+        return conta;
+    }
+
+    public void setConta(Integer conta) {
+        this.conta = conta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void saque(double valor) {
+        saldo = saldo - valor;
+    }
+
+    public double deposito(double valor) {
+        return saldo += valor;
+    }
+
+    public boolean validaSaldo(Double valor) {
+        if ((saldo - valor) < 0) {
+            return false;
+        }
+        return true;
+    }
+
+}
